1f1f091cd7f385c51e2689bf400c5e35b86494ff
Gedikoglu, Ali <ali.gedikoglu@adesso.com.tr>
2024-01-30 09:46:53 +0300

    [feature-movies_page]  widget constants class created.

---
+++ lib/app/core/constants/widgets_constants.dart
@@ -0,0 +1,22 @@
+abstract final class WidgetsConstants {
+  //Carousel
+  static const double carouselCardRightPadding = 20;
+
+  //Movie Cell
+  static const double movieCellHeight = 130;
+  static const double movieCellImageWidth = 100;
+  static const double movieCellBodyPaddingLeft = 18;
+  static const double movieCellDividerHeight = 10;
+  static const double movieCellDividerWidth = 10;
+  static const double movieCellDividerPaddingHorizontal = 6;
+  static const double movieCellDividerPaddingVertical = 10;
+  static const double movieCellCardElevation = 8;
+
+  //Rating View
+  static const double ratingViewPaddingHorizontal = 14;
+  static const double ratingViewPaddingVertical = 6;
+  static const double ratingViewCornerRadius = 20;
+
+  //Release Date View
+  static const double releaseDateViewSizedBoxHegiht = 10;
+}
