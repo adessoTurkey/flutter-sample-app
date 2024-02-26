d676c1b32f94dbe7f8a527cc77f07b78d4c7df89
Gedikoglu, Ali <ali.gedikoglu@adesso.com.tr>
2024-02-27 00:47:56 +0300

    [feature/profile-page-ui] context extension heightFactor updated.

--- lib/app/core/extensions/context_extensions.dart
+++ lib/app/core/extensions/context_extensions.dart
@@ -3,9 +3,9 @@ import 'package:flutter/material.dart';
 extension ContextExtensions on BuildContext {
   Size get screenSize => MediaQuery.of(this).size;
 
-  double heightFactor({double heightFactor = 0}) =>
+  double heightFactor(double heightFactor) =>
       MediaQuery.of(this).size.height * heightFactor;
 
-  double widthFactor({double widthFactor = 0}) =>
+  double widthFactor(double widthFactor) =>
       MediaQuery.of(this).size.width * widthFactor;
 }
